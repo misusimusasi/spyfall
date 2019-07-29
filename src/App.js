bbef22edf9b4b4c881ad4e4a32c5339bd6005ec8
ZolotoArtem <a.zoloto2@gmail.com>
2019-07-29 20:49:15 +0300

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as firebase from "firebase"; // TODO import only firebase/app
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const DBref = firebase.database().ref('rooms');
+  DBref.on("value", function(snapshot) {
+    console.warn(snapshot.val());
+  });
+
+  const writeUserData = () => {
+    firebase.database().ref('rooms/' + 123).set({
+      roomId: 123,
+    });
+  }
+
+  writeUserData();
+  
+  return <div className="App" />;
 }
 
 export default App;
